dd0d590c30b40d71bdf3865aeb4e5e192d890805
Inyeob Kim <deran2016@gmail.com>
2021-04-20 12:14:23 +0900

    init commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class Controller {
+    private String status = null;
+    @PostMapping
+    public void postStatus() {
+        this.status = "posted";
+    }
+    @GetMapping
+    public String getStatus() {
+        return this.status;
+    }
+}
