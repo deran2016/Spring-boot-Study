7713fb2e8d6212ba67567b363b7b579d22422c6b
Inyeob Kim <deran2016@gmail.com>
2021-04-21 11:15:04 +0900

    Add Notice.java

---
+++ src/main/java/com/example/demo/service/Notice.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@Getter
+public class Notice {
+    private int no;
+
+    private String title;
+
+    private String date;
+
+    private int views;
+}
